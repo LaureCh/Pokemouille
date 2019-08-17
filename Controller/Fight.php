b8cba8a8b13968612524bf2b12dad02ed4ef2a10
LaureCh <laure.christol@gmail.com>
2019-08-17 19:45:41 +0200

    client-serveur

---
+++ Controller/Fight.php
@@ -0,0 +1,26 @@
+<?php
+if(!empty($_POST['attack'])){
+  $attack = $_POST['attack'];
+
+  // Search attack on DB
+  $return[] = array();
+  switch ($attack) {
+    case 1:
+      $return["damage"] = 20;
+      echo json_encode($return);
+      break;
+    case 2:
+      $return["damage"] = 40;
+      echo json_encode($return);
+      break;
+    case 3:
+      $return["damage"] = 80;
+      echo json_encode($return);
+      break;
+  }
+}
+else{
+    echo "Attaque introuvable";
+}
+
+?>
--- Controller/Main.php
+++ Controller/Main.php
@@ -1,21 +1,28 @@
 <?php
-include ('..\Model\Pokemon.php');
-include('..\Vue\index.php');
-include('..\Model\Dresseur.php');
+$step=0;
+include ('../Model/Pokemon.php');
+include('../Model/Dresseur.php');
 
-//if (isset($_POST['username'])) {
-//    $dresseur = new Dresseur($_POST['username']);
-//}
+if (isset($_POST['form-pseudo'])) {
+  $dresseur = new Dresseur($_POST['name']);
+  $step = 1;
+}
+if(isset($_POST['form-select'])) {
+  $step = 2;
+}
+if(isset($_POST['form-clear'])) {
+  $step = 0;
+}
+
+include('../Vue/index.php');
 
 $opponentNames = parse_ini_file('../config.ini', true)['OPPONENTS'];
-$randomInt= rand(0, count($opponentNames['opponents']));
+$randomInt= rand(0, count($opponentNames['opponents'])-1);
 $opponent = new Dresseur($opponentNames['opponents'][$randomInt]);
 var_dump($opponent); die;
 
 $pokemonNames = parse_ini_file('./config.ini', true)['POKEMONS'];
 $opponent = 'hello';
-
-
 //$pokemon1 = new Pokemon('pikachu');
 //var_dump($pokemon1);
 //if (isset($_POST['name'])){
@@ -26,6 +33,3 @@ $opponent = 'hello';
 
 
 ?>
-
-
-
